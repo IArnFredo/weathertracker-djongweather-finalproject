1235fa85e14b27805ce09d4311be49b2f56c4e60
asadler <64300912+asadler-spec@users.noreply.github.com>
2020-12-18 04:18:23 +0700

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,13 @@
+export const homeObjOne = {
+  lightBg: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Weather Finder',
+  headline: 'For All Of You That Need Weather',
+  description:
+    'Get access to our exclusive weather finder and make sure u are safe from thunderstorm',
+  imgStart: '',
+  img: 'images/svg-1.svg',
+  alt: 'Credit Card'
+
+};
